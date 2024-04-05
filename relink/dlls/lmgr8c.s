e66952fc44f5cd2a7dbb68729440ea11b7173125
mid-kid <esteve.varela@gmail.com>
2024-04-06 01:44:30 +0200

    Name functions in lmgr8c

--- relink/dlls/lmgr8c.s
+++ relink/dlls/lmgr8c.s
@@ -3,12 +3,18 @@
 
 .global _LMGR8C.dll_be
 _LMGR8C.dll_be:
-    die _LMGR8C.dll_be
+.global lp_checkout
+lp_checkout:
+    die lp_checkout
 
 .global _LMGR8C.dll_bd
 _LMGR8C.dll_bd:
-    die _LMGR8C.dll_bd
+.global lp_checkin
+lp_checkin:
+    die lp_checkin
 
 .global _LMGR8C.dll_bf
 _LMGR8C.dll_bf:
-    die _LMGR8C.dll_bf
+.global lp_errstring
+lp_errstring:
+    die lp_errstring
