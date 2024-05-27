f21f163ae510f05ba87a931e674f1213db9b1354
mid-kid <esteve.varela@gmail.com>
2024-05-27 16:35:30 +0200

    Use WINE headers for type-correctness

--- relink/dlls/kernel32.c
+++ relink/dlls/kernel32.c
@@ -1,13 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// WINE headers, used for the sake of type-checking definitions
+#include "windef.h"
+#include "winbase.h"
+
 #ifdef TRACE
 #define TR(msg, ...) printf("trace: " msg "\n", ##__VA_ARGS__)
 #else
 #define TR(msg, ...)
 #endif
 
-void ExitProcess(unsigned uExitCode)
-{ TR("ExitProcess: uExitCode=%d", uExitCode);
+WINBASEAPI VOID DECLSPEC_NORETURN WINAPI ExitProcess(DWORD uExitCode)
+{ TR("ExitProcess: uExitCode=%lu", uExitCode);
     exit(uExitCode);
 }
