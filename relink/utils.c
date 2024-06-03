589f04e89bb765268b8af02e3304e24cc9b360a2
mid-kid <esteve.varela@gmail.com>
2024-06-03 19:33:00 +0200

    Translate path_dup_unx into c

---
+++ relink/utils.c
@@ -0,0 +1,12 @@
+#include <string.h>
+#include <stdlib.h>
+
+char *path_dup_unx_c(const char *path)
+{
+    const char *o = path;
+    if (o[0] == 'Z' && o[1] == ':' && o[2] == '\\') o += 2;
+
+    char *n = strcpy(malloc(strlen(o) + 1), o);
+    for (char *c = n; *c; c++) if (*c == '\\') *c = '/';
+    return n;
+}
