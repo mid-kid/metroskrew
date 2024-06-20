fe802676368092fbf9525acff0976df247870940
mid-kid <esteve.varela@gmail.com>
2024-06-20 22:30:22 +0200

    WIP

--- scan.c
+++ scan.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 
 struct loc {
     char *name;
@@ -14,6 +15,57 @@ struct file {
     size_t size;
     unsigned char data[];
 };
+
+struct scan {
+    int offset;
+    size_t size;
+    unsigned char *data;
+};
+#define DEF_SCAN(_offset, ...) {\
+    .offset = _offset, \
+    .data = (unsigned char *)&((unsigned char[]){__VA_ARGS__}), \
+    .size = sizeof((unsigned char[]){__VA_ARGS__})}
+#define END_SCAN {}
+
+uint16_t read_u16(const unsigned char *mem)
+{
+    return mem[0] << 0 | mem[1] << 8;
+}
+
+uint32_t read_u32(const unsigned char *mem)
+{
+    return mem[0] << 0 | mem[1] << 8 | mem[2] << 16 | mem[3] << 24;
+}
+
+const unsigned char *scan(const struct file *binary, const struct scan *scan)
+{
+    size_t off = 0;
+    for (;;) {
+        const struct scan *cur = scan;
+
+        // Find the first scan region
+        unsigned char *pos =
+            memmem(binary->data + off, binary->size - off,
+                cur->data, cur->size);
+        if (!pos) return NULL;
+        off = pos - binary->data;
+
+        // Make sure the base offset makes sense
+        size_t scan_base = off - cur->offset;
+        off++;
+        if (scan_base > binary->size) continue;
+
+        // Verify that the rest of the scan regions match
+        while ((++cur)->data) {
+            size_t scan_off = scan_base + cur->offset;
+            if (scan_off > binary->size) break;
+            if (scan_off + cur->size > binary->size) break;
+            if (memcmp(binary->data + scan_off, cur->data, cur->size) != 0) break;
+        }
+        if (cur->data) continue;
+        return binary->data + scan_base;
+    }
+}
 
 unsigned find_fs(const struct file *binary, const struct loc **res)
 {
@@ -64,27 +116,54 @@ unsigned find_init(const struct file *binary, const struct loc **res)
     // Find the entrypoint, and extract the locations of a few functions
     //  as well as addresses that can be found there.
 
+    const struct scan code[] = {
+        // Sentinel code
+        DEF_SCAN(0,
+            0x68, 0x00, 0x00, 0x60, 0x00,  // push 0x600000
+            0xe8                           // call unk
+        ),
+        // Verify the instructions for all the parameters we're extracting
+        DEF_SCAN(-10,
+            0xe8  // call init_args
+        ),
+        DEF_SCAN(-5,
+            0xe8  // call init_envp
+        ),
+        DEF_SCAN(29,
+            0xe8  // call main
+        ),
+        DEF_SCAN(11,
+            0xff, 0x35  // push dword ptr [envp]
+        ),
+        DEF_SCAN(17,
+            0xff, 0x35  // push dword ptr [argv]
+        ),
+        DEF_SCAN(23,
+            0xff, 0x35  // push dword ptr [argc]
+        ),
+        END_SCAN
+    };
+
+    const unsigned char *pos = scan(binary, code);
+
     static struct loc loc[] = {
         {.name = "init_args"},
         {.name = "init_envp"},
         {.name = "main"},
-        {.name = "argc"},
+        {.name = "envp"},
         {.name = "argv"},
-        {.name = "envp"}
+        {.name = "argc"}
     };
 
-    static const char code[] = {
-        0x68, 0x00, 0x00, 0x60, 0x00,  // push 0x600000
-        0xe8                           // call [i32]
-    };
-
-    for (;;) {
-        unsigned char *pos =
-            memmem(binary->data, binary->size, code, sizeof(code));
-        if (!pos) return 0;
-        size_t off = pos - binary->data;
-        if (off < 10) return 0;
-    }
+    if (!pos) return 0;
+    loc[0].start = read_u32(pos - 10 + 1);
+    loc[1].start = read_u32(pos - 5 + 1);
+    loc[2].start = read_u32(pos + 29 + 1);
+    loc[3].start = read_u32(pos + 11 + 2);
+    loc[4].start = read_u32(pos + 17 + 2);
+    loc[5].start = read_u32(pos + 23 + 2);
+    *res = loc;
+    return 6;
 }
 
 typedef unsigned (*funcs_t)(const struct file *, const struct loc **);
@@ -104,8 +183,8 @@ int scan_bin(FILE *out, const struct file *binary)
             const struct loc *loc = patches + i;
             fprintf(out, "\n");
             if (loc->end) {
-                fprintf(out, "patch_%s = 0x%lx\n", loc->name, loc->start);
-                fprintf(out, "patch_%s.end = 0x%lx\n", loc->name, loc->end);
+                fprintf(out, "code_%s = 0x%lx\n", loc->name, loc->start);
+                fprintf(out, "code_%s.end = 0x%lx\n", loc->name, loc->end);
             } else {
                 fprintf(out, "addr_%s = 0x%lx\n", loc->name, loc->start);
             }
