d6caec72191f0a9ecda2f3d2a7841217d15903e5
mid-kid <esteve.varela@gmail.com>
2024-06-22 01:41:29 +0200

    Clean up version.s

--- dlls/version.s
+++ dlls/version.s
@@ -1,7 +1,5 @@
 .intel_syntax noprefix
 .include "_macros.i"
 
-.weak GetFileVersionInfoA
 func GetFileVersionInfoA; die GetFileVersionInfoA
-.weak VerQueryValueA
 func VerQueryValueA; die VerQueryValueA
