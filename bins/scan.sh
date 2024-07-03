9c1075a6c251f6759f3b6007d79c6d41d1f8493c
mid-kid <esteve.varela@gmail.com>
2024-07-03 20:14:41 +0200

    Commit scan.sh

---
+++ bins/scan.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -eu
+
+BINS="${BINS:-mw-executables}"
+
+parse_ver() {
+    sed -n 's/^Version \([0-9\.]*\) build \([0-9]*\).*$/\1-\2/p'
+}
+
+for x in $(cd "$BINS"; find -name '*.exe'); do
+    ver="${x%/*}"
+    ver="${x#./}"
+    name="${x##*/}"
+    name="${name%.*}"
+    sum="$( cd "$BINS"; sha1sum "$x" | cut -f1 -d\ )"
+    build="$(wine "$BINS/$x" | parse_ver)"
+    echo "$name-$build $ver $sum"
+done | LANG=C sort | xargs printf "  ['%s', '%s',\n    '%s'],\n"
