2d6415b6e81d8dc0cb522535dfbd2562cdca5e03
mid-kid <esteve.varela@gmail.com>
2024-07-15 09:48:00 +0200

    Add test for -wrap:sdk option

--- wrap/test.sh
+++ wrap/test.sh
@@ -24,4 +24,12 @@ case "$1" in
                 wine "$PROGRAM_NAME" mwccarm -help -wrap:dbg
         )
         ;;
+
+    sdk)
+        export SKREW_DATADIR="$DIR"
+        cp -av "$PROGRAM" "$DIR"
+        cp -av "$MWCCARM" "$DIR/test.exe"
+        mkdir -p "$DIR/sdk/test"
+        echo test.exe > "$DIR/sdk/test/mwccarm.exe.txt"
+        wine "$DIR/$PROGRAM_NAME" mwccarm -wrap:sdk test -help
 esac
