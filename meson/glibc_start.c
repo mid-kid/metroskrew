0d36fb4f50a2ece121f8806a6c2094bdb915336b
mid-kid <esteve.varela@gmail.com>
2024-07-19 13:08:38 +0200

    Fix infinite loop when built with glibc versions <= 2.34

--- meson/glibc_start.c
+++ meson/glibc_start.c
@@ -1,7 +1,10 @@
 #ifdef __i386__
+#include <features.h>
+#if __GLIBC__ > 2 || __GLIBC_MINOR__ >= 34
 // Link against the old __libc_start_main
 __asm__(".symver old__libc_start_main, __libc_start_main@GLIBC_2.0");
 __asm__(".symver new__libc_start_main, __libc_start_main@@_NEW");
 __asm__(".global new__libc_start_main");
 __asm__("new__libc_start_main: jmp old__libc_start_main@PLT");
+#endif
 #endif
