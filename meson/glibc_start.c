ddf44ce2465e41b0f03fd08b7226d04fb8315e44
mid-kid <esteve.varela@gmail.com>
2024-09-15 12:15:26 +0200

    Better streamline linux and windows binary layouts

--- meson/glibc_start.c
+++ meson/glibc_start.c
@@ -5,6 +5,6 @@
 __asm__(".symver old__libc_start_main, __libc_start_main@GLIBC_2.0");
 __asm__(".symver new__libc_start_main, __libc_start_main@@_NEW");
 __asm__(".global new__libc_start_main");
-__asm__("new__libc_start_main: jmp old__libc_start_main@PLT");
+__asm__("new__libc_start_main: jmp old__libc_start_main");
 #endif
 #endif
