7d970ba9d53b29061cffda930232802b8b98debb
mid-kid <esteve.varela@gmail.com>
2024-11-04 23:21:49 +0100

    Strip the binaries

--- releng/release_linux.sh
+++ releng/release_linux.sh
@@ -10,13 +10,15 @@ rm -rf build_linux
 "$MESON" setup "$PROJ" build_linux \
     --cross-file "$PROJ/meson/i686-unknown-linux-gnu.ini" \
     --prefix / \
-    --buildtype release
+    --buildtype release \
+    --strip
 "$MESON" install -C build_linux --destdir ../install_linux
 rm -rf build_linux
 
 mv install_linux metroskrew
 cp -aT install_libs metroskrew
 
+rm -f metroskrew-linux.tar metroskrew-linux.tar.xz
 tar cf metroskrew-linux.tar metroskrew
 rm -rf metroskrew
 xz -9v metroskrew-linux.tar
--- releng/release_windows.sh
+++ releng/release_windows.sh
@@ -10,12 +10,15 @@ rm -rf build_windows
 "$MESON" setup "$PROJ" build_windows \
     --cross-file "$PROJ/meson/i686-w64-mingw32.ini" \
     --prefix / \
-    --buildtype release
+    --buildtype release \
+    --strip
 "$MESON" install -C build_windows --destdir ../install_windows
 rm -rf build_windows
 
 mv install_windows metroskrew
 cp -aT install_libs metroskrew
+
+rm -f metroskrew-windows.7z
 7zz a -mx9 metroskrew-windows.7z metroskrew || \
     7z a -mx9 metroskrew-windows.7z metroskrew
 rm -rf metroskrew
